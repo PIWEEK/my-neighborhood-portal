fc7b12a450780047944e0b333d085c1d90056cae
Andrés Moya <andres.moya@kaleidos.net>
2020-12-16 09:39:18 +0100

    Read Facebook posts

--- backend/directory/management/commands/update_posts.py
+++ backend/directory/management/commands/update_posts.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from datetime import datetime, timezone
 from directory.models import Entity, SocialNetwork, SocialPost
 
 import requests
 import feedparser
-import datetime
 
 
 class Command(BaseCommand):
@@ -15,7 +15,6 @@ class Command(BaseCommand):
             for network in entity.networks.all():
                 self._update_posts(entity, network)
 
-
     def _update_posts(self, entity, network):
         posts = []
 
@@ -23,7 +22,8 @@ class Command(BaseCommand):
             posts = self._read_atom_feed(network)
         elif network.network_type == "twitter":
             posts = self._read_twitter_timeline(network)
-
+        elif network.network_type == "facebook":
+            posts = self._read_facebook_wall(network)
 
     def _read_atom_feed(self, network):
         result = feedparser.parse(network.url + "/feeds/posts/default")
@@ -39,7 +39,7 @@ class Command(BaseCommand):
             post = SocialPost.objects.create(
                 network = network,
                 url = entry.link,
-                date = _tuple_to_datetime(entry.published_parsed),
+                date = _rss_date_to_datetime(entry.published_parsed),
                 image_url = image_url,
                 text = entry.summary,
             )
@@ -49,10 +49,10 @@ class Command(BaseCommand):
         user_id = network.url[len("https://twitter.com/"):]
         api_url = "https://api.twitter.com/2/tweets/search/recent"
         params = {
+            "query": "from:%s" % user_id,
             "tweet.fields": "attachments,created_at",
             "media.fields": "preview_image_url,url",
             "expansions": "attachments.media_keys",
-            "query": "from:%s" % user_id
         }
         headers = {
             "Authorization": "Bearer %s" % settings.TWITTER_BEARER_TOKEN,
@@ -74,21 +74,47 @@ class Command(BaseCommand):
             post = SocialPost.objects.create(
                 network = network,
                 url = "https://twitter.com/%s/status/%s" % (user_id, tweet["id"]),
-                date = _isoformat_to_datetime(tweet["created_at"]),
+                date = _twitter_date_to_datetime(tweet["created_at"]),
                 image_url = image["url"] if image else "",
                 text = tweet.get("text", ""),
             )
             print(post)
 
+    def _read_facebook_wall(self, network):
+        user_id = network.url[len("https://www.facebook.com/"):]
+        api_url = "https://graph.facebook.com/%s/feed" % user_id
+        params = {
+            "access_token": settings.FACEBOOK_ACCESS_TOKEN,
+            "fields": "permalink_url,created_time,message,full_picture",
+            "limit": 10,
+        }
+        result = requests.get(api_url, params=params).json()
 
-def _tuple_to_datetime(time_tuple):
-    return datetime.datetime(
+        SocialPost.objects.filter(network=network).delete()
+
+        for post in result.get("data", []):
+            post = SocialPost.objects.create(
+                network = network,
+                url = post.get("permalink_url", ""),
+                date = _facebook_date_to_datetime(post.get("created_time", "")),
+                image_url = post.get("full_picture", ""),
+                text = post.get("message", ""),
+            )
+            print(post)
+
+
+def _rss_date_to_datetime(time_tuple):
+    return datetime(
         *time_tuple[0:6],
         0,
-        datetime.timezone.utc
+        timezone.utc
     )
 
 
-def _isoformat_to_datetime(isoformat):
-    return datetime.datetime.fromisoformat(isoformat[:-1] + "+00:00")
+def _twitter_date_to_datetime(isoformat):
+    return datetime.fromisoformat(isoformat[:-1] + "+00:00")
+
+
+def _facebook_date_to_datetime(isoformat):
+    return datetime.fromisoformat(isoformat[:-2] + ":00")
 
